31e103b6ffe9c29d458d3ddbb2ad7bb4c38df534
ronz <support@dboxshare.com>
2020-11-22 18:44:02 +0800

    first commit

--- dist/www/web/js/base-data.js
+++
@@ -1,6 +0,0 @@
-﻿var myId = {userid};
-var myUsername = '{username}';
-var myAdmin = {useradmin};
-var systemDate = new Date('{systemdate}');
-var uploadExtension = '{uploadextension}';
-var uploadSize = {uploadsize};
